3c009bca4e407797146eb74aa57644a98bba9730
artyom-shost <163983821+artyom-shost@users.noreply.github.com>
2025-03-24 14:46:42 +0700

    task2.py

--- task2.py
+++ task2.py
@@ -1,9 +1,10 @@
+import math
 a = float(input("Введите высоту отверстия: "))
 b = float(input("Введите ширину отверстия: "))
 c = float(input("Введите введите длину кирпича: "))
 d = float(input("Введите введите ширину кирпича: "))
 e = float(input("Введите введите высоту кирпича: "))
-if ((a >= c and b >= d) or (a >= d and b >= c) or (a >= d and b >= e) or (a >= e and b >= d) or (a >= c and b >= e) or (a >= e and b >= c)):
+if (math.sqrt(a ** 2 + b ** 2) >= math.sqrt(c ** 2 + d ** 2)) or (math.sqrt(a ** 2 + b ** 2) >= math.sqrt(c ** 2 + e ** 2)) or (math.sqrt(a ** 2 + b ** 2) >= math.sqrt(d ** 2 + e ** 2)):
     print("да")
 else:
     print("нет")
