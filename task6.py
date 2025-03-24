9d7488b58ef4ad9f66816fc4514baef710e14e2d
artyom-shost <163983821+artyom-shost@users.noreply.github.com>
2025-03-24 14:52:19 +0700

    task6.py

--- task6.py
+++ task6.py
@@ -1,6 +1,9 @@
 n = int(input("Введите количество детей (N): "))
 k = int(input("Введите вместимость карусели (K): "))
 m = int(input("Введите длительность сеанса (M): "))
-rides = (n * 2 + k - 1) // k
-total_time = rides * m
+if n <= k:
+    total_time = m * 2
+else:
+    rides = (n * 2 + k - 1) // k
+    total_time = rides * m
 print(total_time)
